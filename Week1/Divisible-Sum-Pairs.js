7baa9a3e2fa130d8382a9bb99ef6e1cafa16cd41
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-08-02 20:51:12 +0900

    create: add Divisible Sum Pairs solution

---
+++ Week1/Divisible-Sum-Pairs.js
@@ -0,0 +1,22 @@
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+function divisibleSumPairs(n, k, ar) {
+  // Write your code here
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        cnt++;
+      }
+    }
+  }
+  return cnt;
+}
