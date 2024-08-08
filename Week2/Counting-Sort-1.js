642442793f39afb04d3313b0a32221467abc6f94
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-08-08 14:24:25 +0900

    create: add Counting Sort 1 solution

---
+++ Week2/Counting-Sort-1.js
@@ -0,0 +1,18 @@
+/*
+Problem:
+Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+*/
+
+
+/* Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {  
+    const list = new Array(100).fill(0)
+    arr.forEach(num => list[num]++)
+
+    return list
+}
